cb45135a50a192520f96fa92f5ec1469ad3d5053
junhong.lee429@gmail.com <junhong.lee429@gmail.com>
2026-02-08 14:35:10 +0900

    Implement water caustics effect

---
+++ src/Experience.jsx
@@ -0,0 +1,17 @@
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import WaterCaustics from './WaterCaustics'
+
+export default function Experience() {
+  return (
+    <>
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      <ambientLight intensity={0.5} />
+
+      <WaterCaustics position={[0, 0, 0]} scale={10} />
+    </>
+  )
+}
