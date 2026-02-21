617bab12284ef4f9ec42cc66ea3a4533ed962a7c
junhong.lee429@gmail.com <junhong.lee429@gmail.com>
2026-02-21 12:17:32 +0900

    Set background as black

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import Experience from "./experience";
 export default function App() {
   return (
     <Canvas
+      style={{ background: "#000" }}
       camera={{
         fov: 45,
         near: 0.1,
         far: 200,
-        position: [3, 2, 6],
+        position: [8, 12, 12],
       }}
     >
       <Experience />
